3d2cec6e4ab46d062a7ad85d8330d659261eb536
Francisco Pereira <francisco.anpereira@gmail.com>
2018-06-03 20:57:38 -0300

    endpoint categorias criado

---
+++ src/main/java/br/com/api/apirest/resource/CategoriaResource.java
@@ -0,0 +1,26 @@
+package br.com.api.apirest.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.apirest.model.Categoria;
+import br.com.api.apirest.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listarTodos() {
+		
+		return categoriaRepository.findAll();
+	}
+	
+}
