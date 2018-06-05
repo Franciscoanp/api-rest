1e9dd258714fe1e69aa8f58f30ba822c1f19e31b
Francisco Pereira <francisco.anpereira@gmail.com>
2018-06-04 23:58:42 -0300

    classe de excecao e validacao de entidade null

--- src/main/java/br/com/api/apirest/resource/CategoriaResource.java
+++ src/main/java/br/com/api/apirest/resource/CategoriaResource.java
@@ -1,11 +1,22 @@
 package br.com.api.apirest.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.api.apirest.model.Categoria;
 import br.com.api.apirest.repository.CategoriaRepository;
@@ -16,10 +27,33 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@GetMapping
 	public List<Categoria> listarTodos() {
 		return categoriaRepository.findAll();
 	}
-	
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+
+	@GetMapping("/{codigo}")
+	public ResponseEntity<?> buscarPeloCodigo(@PathVariable Long codigo) {
+		 Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+		 
+		 if(categoria.isPresent()) {
+			 return ResponseEntity.ok().body(categoria.get());
+		 }
+		 
+		 return  ResponseEntity.notFound().build();
+	}
+
 }
