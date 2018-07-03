1c33c2ee461999ce97dcf0d6d06b4ad7ccc3c676
Francisco Pereira <francisco.anpereira@gmail.com>
2018-07-03 19:34:45 -0300

    endpoint Pessoa - consulta e atualizacao criados

--- src/main/java/br/com/api/apirest/resource/CategoriaResource.java
+++ src/main/java/br/com/api/apirest/resource/CategoriaResource.java
@@ -8,14 +8,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -35,26 +35,27 @@ public class CategoriaResource {
 	}
 
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
 				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+
+		// informa o location após criar uma nova categoria
 		response.setHeader("Location", uri.toASCIIString());
 
 		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
 
-	@GetMapping("/{codigo}")
-	public ResponseEntity<?> buscarPeloCodigo(@PathVariable Long codigo) {
-		 Optional<Categoria> categoria = categoriaRepository.findById(codigo);
-		 
-		 if(categoria.isPresent()) {
-			 return ResponseEntity.ok().body(categoria.get());
-		 }
-		 
-		 return  ResponseEntity.notFound().build();
+	@GetMapping(path = "/{codigo}")
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+
+		if (categoria.isPresent()) {
+			return ResponseEntity.ok().body(categoria.get());
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 
 }
